9cfca21bf9d1f7835837fa8b3ef23ebce63dc81f
mhdyzrn <51892371+mhdyzrn@users.noreply.github.com>
2019-12-23 00:50:01 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 9; Mi A1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36";
+$Email = "mhdyuzron@gmail.com";
+$Password = "123yusron";
+
+//Masukkan Cookie -> cf_clearance atau PHPSESSID
+$Cookie = "5auprf3lt6i2qk3rffk90bh207";
